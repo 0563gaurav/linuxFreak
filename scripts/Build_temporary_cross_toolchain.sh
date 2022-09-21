1265240d5c8374fd64bf32c11f4b3c6be43f1817
Gaurav Gautam Shakya <gaurav@kali.grvKali.com>
2022-09-21 20:44:25 +0530

    Build_temporary_cross_toolchain.sh updated

--- scripts/Build_temporary_cross_toolchain.sh
+++ scripts/Build_temporary_cross_toolchain.sh
@@ -35,11 +35,11 @@ echo
 echo 
 echo 
 echo
-echo "Starting with gcc package ...." 1> $LOG/current_pkg
+echo "Starting with gcc package ...."
 sleep 3
 
 
-tar -xvf gcc-10.2.0.tar.xz 2> $LOG/err
+tar -xvf gcc-10.2.0.tar.xz 
 cd gcc-10.2.0 
 #? dependency resolve 
 #gcc package dependes on mpc , mpfr and gmp packages 
@@ -77,13 +77,13 @@ cd build
 	--disable-libssp	  \
 	--disable-libvtv	  \
 	--disable-libstdcxx	  \
-	--enable-lanuages=c,c++  2> $LOG/err
-make -j8 2> $LOG/err
-make install 2> $LOG/err
+	--enable-lanuages=c,c++  
+make -j8 
+make install 
 #Note 
 cd ../
 cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
-	`dirname $($LFS_TGT-gcc -print-libgcc-file-name) ` /install-tools/include/limits.h 2>$LOG/err
+	`dirname $($LFS_TGT-gcc -print-libgcc-file-name) ` /install-tools/include/limits.h 
 
 cd ../
 rm -rf gcc-10.2.0
@@ -92,16 +92,16 @@ rm -rf gcc-10.2.0
 
 
 #/* linux API  headers */
-echo "stating with installing linux API" 1>$LOG/current_pkg
+echo "stating with installing linux API" 
 sleep 3
 
-tar -xvf linux-5.8.3.tar.xz 2> $LOG/err
+tar -xvf linux-5.8.3.tar.xz 
 cd linux-5.8.3/
 make mrproper 2>$LOG/err
 make headers 2>$LOG/err
-find usr/include -name '.*' -delete 2>$LOG/err
-rm -rf usr/include/Makefile 2>$LOG/err
-cp -rv usr/include/ $LFS/usr 2>$LOG/err
+find usr/include -name '.*' -delete 
+rm -rf usr/include/Makefile 
+cp -rv usr/include/ $LFS/usr
 
 cd ../
 rm -rf linux-5.8.3
@@ -111,21 +111,21 @@ rm -rf linux-5.8.3
 
 
 #/*GLIBC */
-echo "starting with GLIBC " 1> $LOG/err
+echo "starting with GLIBC " 
 sleep 3
 
 #? creating a symbolic link for LSB compliance 
 case $(uname -r) in
-	i?86) ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3  2> $LOG/err ;; 
-	x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64 2>$LOG/err
-		ln -svf ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x8-64.so3 2> $LOG/err
+	i?86) ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3   ;; 
+	x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64 
+		ln -svf ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x8-64.so3 
 		;;
 esac 
 
 
 
 
-tar -xvf glibc-2.32.tar.xz 2>$LOG/err
+tar -xvf glibc-2.32.tar.xz 
 cd glibc-2.32
 #Patching the glibc
 patch -Np1 -i ../glibc-2.32-fhs-1.patch
@@ -138,20 +138,20 @@ cd build
 	--build=$(./scripts/config.guess)	\
 	--enable-kernel=3.2	\
 	--with-headers=$LFS/usr/include		\
-	lib_cv_slibdir=/lib  2>$LOG/err
+	lib_cv_slibdir=/lib  
 
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+make -j8
+make DESTDIR=$LFS install 
 
 cd ../..
 rm -rf glibc-2.32
 
 #At this point of time to stop and ensure that the basic function( compling and linking ) of the new programs are working as expected.
 #To perform the sanity check , run the following commands
-echo "Perfoeming test on toolchain " 1>$LOG/current_pkg
-echo 'init main() {}' >dummy.c 2>$LOG/err
-$LFS_TGT-gcc dummy.c 2>$LOG/err
-readelf -l a.out | grep '/ld-linux' 2>$LOG/err
+echo "Perfoeming test on toolchain " 
+echo 'init main() {}' >dummy.c 
+$LFS_TGT-gcc dummy.c
+readelf -l a.out | grep '/ld-linux' 
 #put put of the program should be " [Requesting program interpreter: /lib64/ld-linux-x86-64.so.2 ]
 rm  dummy.c a.out
 
@@ -160,13 +160,13 @@ rm  dummy.c a.out
 #? Now that our toolchain is complete.
 #finilize the installation of limits.h header.
 #for this run the utility 
-$LFS/tools/libexec/gcc/$LFS_TGT/10.2/install-tools/mkheaders 2>$LOG/err
+$LFS/tools/libexec/gcc/$LFS_TGT/10.2/install-tools/mkheaders 
 
 
 #/* libstdc++ from GCC-10.2.0, pass 1
-echo " libstdc++ is being build ..." 1>$LOG/current_pkg
+echo " libstdc++ is being build ..." 
 sleep 3
-tar -xvf gcc-10.2.0.tar.xz 2> $LOG/err
+tar -xvf gcc-10.2.0.tar.xz 
 cd gcc-10.2.0
 mkdir build
 cd build
@@ -177,34 +177,34 @@ cd build
 	--disable-multilib		\
 	--disable-nls			\
 	--disbale-libstdcxx-pch		\
-	--with-gxx-include-dir=/tools/$LFS_TGT/include/c++/10.2.0  2>$LOG/err
+	--with-gxx-include-dir=/tools/$LFS_TGT/include/c++/10.2.0  
 
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+make -j8 
+make DESTDIR=$LFS install 
 
 cd ../../
 rm -rf gcc-10.2.0
 
 #-------------------------------------------------------------------compiling the temporary tools ------------------------------------------------------------------
-echo "Started compiling the temporary tools" 1> $LOG/current_pkg
+echo "Started compiling the temporary tools" 
 sleep 5
 
 #1. /* M4 (macro processor )*/
 #first make some fixes introduces by glibc-2.28
-echo "M4 Macro prodessor is being build ..." 1> $LOG/current_pkg
+echo "M4 Macro prodessor is being build ..." 
 sleep 3
-sed -i 's/IO-ftrylockfile/IO_EOF_SEEN/' lib/*.c 2> $LOG/err
-echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h  2> $LOG/err
+sed -i 's/IO-ftrylockfile/IO_EOF_SEEN/' lib/*.c 
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h 
 #let's compile the M4
-tar -xvf m4-1.4.18.tar.xz  2> $LOG/err
+tar -xvf m4-1.4.18.tar.xz 
 cd m4-1.4.18
 ./configure 			\
 	--prefix=/usr		\
 	--host=$LFS_TGT		\
-	--build=$(build-aux/cinfig.guess) 2> $LOG/err
+	--build=$(build-aux/cinfig.guess) 
 
-make -j8 2> $LOG/err
-make DESTDIR=$LFS install 2> $LOG/err
+make -j8 
+make DESTDIR=$LFS install 
 
 cd ../
 rm -rf m4-1.4.18
@@ -212,20 +212,20 @@ rm -rf m4-1.4.18
 
 
 #2. /*Ncurces */
-echo "Ncurses is being build ..." 1> $LOG/current_pkg
+echo "Ncurses is being build ..." 
 sleep 3
 #first, ensure that gawk is found first during configuration 
-sed -i s/mawk// configure  2> $LOG/err
+sed -i s/mawk// configure  
 #then, run the following commands to build the "tic" program on the build host
 mkdir build 
-pushd build 2> $LOG/err
-../configure  2> $LOG/err
-make -C include 2> $LOG/err
-make -C progs tic 2>LOG/err
+pushd build 
+../configure  
+make -C include 
+make -C progs tic 
 popd
 
 #compiling the ncurses
-tar -xvf ncurses-6.2.tar.gz 2>$LOG/err
+tar -xvf ncurses-6.2.tar.gz
 cd ncurses-6.2
 ./configure 			\
 	--prefix=/usr		\
@@ -238,243 +238,243 @@ cd ncurses-6.2
 	--without-debug		\
 	--without-ada		\
 	--without-normal	\
-	--enable-widec	2> $LOG/err	
+	--enable-widec	
 
 make -j8 2> $LOG/err
-make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic/ install 2> $LOG/err
-echo "INPUT(-lncursesw)" > $LFS/usr/lib/libncurses.so 2> $LOG/err
+make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic/ install 
+echo "INPUT(-lncursesw)" > $LFS/usr/lib/libncurses.so 
 mv -v $LFS/usr/lib/incursesw.so.6* $LFS/lib 2>$LOG/err
-ln -sfv ../../lib/$(readlink $LFS/usr/lib/libncursesw.so) $LFS/usr/lib/ncursesw.so 2> $LOg/err
+ln -sfv ../../lib/$(readlink $LFS/usr/lib/libncursesw.so) $LFS/usr/lib/ncursesw.so 
 
 cd ../
 rm -rf ncurses-6.2
 
 #/*Bash */
-echo " Bash is being build ..." 1> $LOG/current_pkg
+echo " Bash is being build ..." 
 sleep 3
-tar -xvf bash-5.0.tar.gz 2> $LOG/err
+tar -xvf bash-5.0.tar.gz
 cd bash-5.0
 ./configure 			\
 	--prefix=/usr 		\
 	--build=$(support/config.guess)	\
 	--host=$LFS_TGT			\
-	--without-bash-malloc 2> $LOG/err
-make -j8 2> $LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--without-bash-malloc 
+make -j8 
+make DESTDIR=$LFS install
 
 #move the ececutable as expected 
-mv $LFS/usr/bin/bash $LFS/bin/bash 2>$LOG/err
-ln -sv bash $LFS/bin/sh 2>$LOG/err
+mv $LFS/usr/bin/bash $LFS/bin/bash 
+ln -sv bash $LFS/bin/sh 
 
 cd ../
 rm -rf bash-5.0
 
 #/*Coreutils */
-echo "Coreutils is being build ..." 1>$LOG/current_pkg
+echo "Coreutils is being build ..." 
 sleep 3
-tar -xvf coreutils-8.32.tar.xz 2>$LOG/err
+tar -xvf coreutils-8.32.tar.xz 
 cd coreutils-8.32
 ./configure 			\
 	--prefix=/usr		\
 	--host=$LFS_TGT		\
 	--build=$(build-aux/config.guess)	\
 	--enable-install-program=hostname 	\
-	--enable-no-install-program=kill,uptime 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--enable-no-install-program=kill,uptime 
+make -j8 
+make DESTDIR=$LFS install 
 #move programs in final expected location
 
-mv -v $LFS/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} $LFS/bin 2>$LOG/err
-mv -v $LFS/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} $LFS/bin  2>$LOG/err
-mv -v $LFS/usr/bin/{rmdir,stty,sync,true,uname}  $LFS/bin  2>$LOG/err
-mv -v $LFS/usr/bin/{head,nice,sleep,touch}  $LFS/bin 2>$LOG/err
-mv -v $LFS/usr/bin/chroot   $LFS/usr/sbin 2>$LOG/err
-mkdir -pv $LFS/usr/share/man/man8  2>$LOG/err
-mv -v $LFS/usr/share/man/man1/chroot.1   $LFS/usr/share/man/man8 2>$LOG/err
-sed -i 's/"1"/"8"/' $LFS/usr/share/man/man8 2>$LOG/err
+mv -v $LFS/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} $LFS/bin 
+mv -v $LFS/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} $LFS/bin  
+mv -v $LFS/usr/bin/{rmdir,stty,sync,true,uname}  $LFS/bin  
+mv -v $LFS/usr/bin/{head,nice,sleep,touch}  $LFS/bin 
+mv -v $LFS/usr/bin/chroot   $LFS/usr/sbin 
+mkdir -pv $LFS/usr/share/man/man8  
+mv -v $LFS/usr/share/man/man1/chroot.1   $LFS/usr/share/man/man8 
+sed -i 's/"1"/"8"/' $LFS/usr/share/man/man8 
 
 cd ../
 rm -rf coreutils-8.32
 
 #/*Diffutils */
-echo "Diffutils is being build..." 1> $LOG/current_pkg
+echo "Diffutils is being build..." 
 sleep 3
-tar -xvf diffutils-3.7.tar.xz 2>$LOG/err
+tar -xvf diffutils-3.7.tar.xz 
 cd diffutils-3.7
-./configure --prefix=/usr --host=$LFS_TGT 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+./configure --prefix=/usr --host=$LFS_TGT 
+make -j8 
+make DESTDIR=$LFS install 
 
 cd ../
 rm -rf diffutils-3.7
 
 
 #/*File */
-echo "File is being build ...." 1>$LOG/current_pkg
+echo "File is being build ...." 
 sleep 3
-tar -xvf file-5.39.tar.gz 2>$LOG/err
+tar -xvf file-5.39.tar.gz 
 cd file-5.39
 ./configure --prefix=/usr \
-	--host=$LFS_TGT 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--host=$LFS_TGT 
+make -j8 
+make DESTDIR=$LFS install
 cd ../
 rm -rf file-5.39
 
 
 #/*Findutils */
-echo "Findutils is being build ..." 1>$LOG/current_pkg
+echo "Findutils is being build ..." 
 sleep 3
-tar -xvf findutils-4.7.0.tar.xz 2>$LOG/err
+tar -xvf findutils-4.7.0.tar.xz 
 cd findutils-4.7.0
 ./configure --prefix=/usr \
 	--host=$LFS_TGT  \
-	--build=$(build-aux/config.guess) 2> $LOG/err
+	--build=$(build-aux/config.guess) 
 make -j8  2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+make DESTDIR=$LFS install
 
 #Move the executable to its final expected location:
-mv -v $LFS/usr/bin/find $LFS/bin 2>$LOG/err
-sed -i 's|find:=${BINDIR}|find:=/bin|' $LFS/usr/bin/updatedb 2>$LOG/err
+mv -v $LFS/usr/bin/find $LFS/bin 
+sed -i 's|find:=${BINDIR}|find:=/bin|' $LFS/usr/bin/updatedb 
 
 cd ../
 rm -rf findutils-4.7.0
 
 
 #/*Gawk */
-echo "Gawk is being build ...." 1>$LOG/current_pkg
+echo "Gawk is being build ...." 
 sleep 3
-tar -xvf gawk-5.1.0.tar.xz 2>$LOG/err
+tar -xvf gawk-5.1.0.tar.xz 
 cd gawk-5.1.0
 ./configure --prefix=/usr  \
 	--host=$LFS_TGT	\
-	--build=$(./config.guess) 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--build=$(./config.guess)
+make -j8 
+make DESTDIR=$LFS install 
 
 cd ../
 rm -rf gawk-5.1.0
 
 
 #/*Grep */
-echo "Grep is being build ..." 1>$LOG/current_pkg
+echo "Grep is being build ..." 
 sleep 3 
-tar -xvf grep-3.4.tar.xz 2>$LOG/err
+tar -xvf grep-3.4.tar.xz 
 cd grep-3.4
 ./configure --prefix=/usr  	\
 	--host=$LFS_TGT		\
-	--bindir=/bin 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--bindir=/bin
+make -j8 
+make DESTDIR=$LFS install 
 
 cd ../
 rm -rf grep-3.4
 
 
 #/*Gzip */
-echo "Gzip is being build ..." 1>$LOG/current_pkg
+echo "Gzip is being build ..." 
 sleep 3
-tar -xvf gzip-1.10.tar.xz 2>$LOG/err
+tar -xvf gzip-1.10.tar.xz 
 cd gzip-1.10
 ./configure --prefix=/usr 	\
-	--host=$LFS_TGT	 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--host=$LFS_TGT	 
+make -j8
+make DESTDIR=$LFS install 
 
 #Move the executable to its final expected location:
-mv -v $LFS/usr/bin/gzip $LFS/bin 2>$LOG/err
+mv -v $LFS/usr/bin/gzip $LFS/bin
 
 cd ../
 rm -rf gzip-1.10
 
 
 #/* Make */
-echo "Make is being buils ..." 1>$LOG/current_pkg
+echo "Make is being buils ..." 
 sleep 3
-tar -xvf make-4.3.tar.gz 2>$LOG/err
+tar -xvf make-4.3.tar.gz 
 cd make-4.3
 ./configure --prefix=/usr 	\
 	--without-guile		\
 	--host=$LFS_TGT		\
-	--build=$(build-aux/config.guess) 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--build=$(build-aux/config.guess) 
+make -j8 
+make DESTDIR=$LFS install 
 
 cd ../
 rm -rf make-4.3
 
 
 #/*Patch  */
-echo "Patch is being build ..." 1>$LOG/current_pkg
+echo "Patch is being build ..." 
 sleep 3
-tar -xvf patch-2.7.6.tar.xz 2>$LOG/err
+tar -xvf patch-2.7.6.tar.xz 
 cd patch-2.7.6
 ./configure --prefix=/usr  	\
 	--host=$LFS_TGT		\
-	--build=$(build-aux/config.guess) 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--build=$(build-aux/config.guess)
+make -j8 
+make DESTDIR=$LFS install 
 
 cd ../
 rm -rf patch-2.7.6
 
 
 #/*Sed */
-echo "Sed is being build " 1> $LOG/current_pkg
+echo "Sed is being build " 
 sleep 3
-tar -xvf sed-4.8.tar.xz 2>$LOG/err
+tar -xvf sed-4.8.tar.xz
 cd sed-4.8
 ./configure --prefix=/usr 	\
 	--host=$LFS_TGT 	\
-	--bindir=/bin	2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>LOG/err
+	--bindir=/bin	
+make -j8 
+make DESTDIR=$LFS install 
 
 cd ../
 rm -rf sed-4.8
 
 
 #/*Tar */
-echo " Tar is being build ..." 1>$LOG/current_pkg
+echo " Tar is being build ..." 
 sleep 3
-tar -xvf tar-1.32.tar.xz 2>$LOG/err
+tar -xvf tar-1.32.tar.xz 
 cd tar-1.32
 ./configure --prefix=/usr 	\
 	--host=$LFS_TGT		\
 	--build=$(build-aux/config.guess)	\
-	--bindir=/bin 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOg/err
+	--bindir=/bin 
+make -j8 
+make DESTDIR=$LFS install 
 
 cd ../
 rm -rf tar-1.32
 
 
 #/*XZ */
-echo "XZ is being build ..." 1>$LOG/currnet_pkg
+echo "XZ is being build ..." 
 sleep 3
-tar -xvf xz-5.2.5.tar.xz 2>$LOG/err
+tar -xvf xz-5.2.5.tar.xz
 cd xz-5.2.5
 ./configure --prefix=/usr 	\
 	--host=$LFS_TGT		\
 	--build=$(build-aux/config.guess)	\
 	--disable-static 		\
-	--docdir=/usr/share/doc/xz-5.2.5 2>LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--docdir=/usr/share/doc/xz-5.2.5
+make -j8 
+make DESTDIR=$LFS install 
 
 #Make sure that all essential files are in the correct directory:
-mv -v $LFS/usr/bin/{lzma,unlzma,lzcat,xz,unxz,xzcat} $LFS/bin 2>$LOG/err
-mv -v $LFS/usr/lib/liblzma.so.*  $LFS/lib 2>$LOG/err
-ln -svf ../../lib/$(readlink $LFS/usr/lib/liblzma.so) $LFS/usr/lib/liblzma.s 2>$LOG/err
+mv -v $LFS/usr/bin/{lzma,unlzma,lzcat,xz,unxz,xzcat} $LFS/bin 
+mv -v $LFS/usr/lib/liblzma.so.*  $LFS/lib 
+ln -svf ../../lib/$(readlink $LFS/usr/lib/liblzma.so) $LFS/usr/lib/liblzma.s 
 
 cd ../
 rm -rf xz-5.2.5
 
 #/*Binutils PASS 2  */
-echo "Binutils , PASS-2 , is being build ...." 1>$LOG/current_pkg
+echo "Binutils , PASS-2 , is being build ...." 
 sleep 5
-tar -xvf binutils-2.35.tar.xz 2>$LOG/err
+tar -xvf binutils-2.35.tar.xz 
 cd binutils-2.35 
 mkdir -pv  build
 cd  build
@@ -484,30 +484,30 @@ cd  build
 	--disable-nls	\
 	--enable-shared		\
 	--disable-werror	\
-	--enable-64-bit-bfd	2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--enable-64-bit-bfd	
+make -j8 
+make DESTDIR=$LFS install 
 
 
 #/* Gcc PASS-2 */
-echo " GCC , PASS-2 , is being build ...." 1>$LOG/current_pkg
+echo " GCC , PASS-2 , is being build ...." 
 sleep 5 
-tar -xvf gcc-10.2.0.tar.xz 2>$LOG/err
+tar -xvf gcc-10.2.0.tar.xz 
 cd gcc-10.2.0
 #As in the first build of GCC, the GMP, MPFR, and MPC packages are required. Unpack the tarballs and move them into the required directory names:
 
-tar -xvf ../mpfr-4.1.0.tar.xz 2>$LOG/err
+tar -xvf ../mpfr-4.1.0.tar.xz 
 mv -v mpfr-4.1.0 mpfr
-tar -xvf ../gmp-6.2.0.tar.xz 2>$LOG/err
+tar -xvf ../gmp-6.2.0.tar.xz 
 mv -v gmp-6.2.0 gmp
-tar -xvf ../mpc-1.1.0.tar.gz 2>$LOG/err
+tar -xvf ../mpc-1.1.0.tar.gz 
 mv -v mpc-1.1.0 mpc
 
 #If building on x86_64, change the default directory name for 64-bit libraries to lib
 
 case $(uname -m) in
 x86_64)
-sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64 2>$LOG/err
+sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
 ;;
 esac
 
@@ -515,7 +515,7 @@ mkdir -pv build
 cd build 
 
 mkdir -pv $LFS_TGT/libgcc
-ln -s ../../../libgcc/gthr-posix.h $LFS_TGT/libgcc/gthr-default.h 2>$LOG/err
+ln -s ../../../libgcc/gthr-posix.h $LFS_TGT/libgcc/gthr-default.h 
 ../configure  --build=$(../config.guess) \
 	--host=$LFS_TGT 	\
 	--prefix=/usr	\
@@ -531,20 +531,20 @@ ln -s ../../../libgcc/gthr-posix.h $LFS_TGT/libgcc/gthr-default.h 2>$LOG/err
 	--disable-libssp		\
 	--disable-libvtv		\
 	--disable-libstdcxx		\
-	--enable-languages=c,c++ 2>$LOG/err
-make -j8 2>$LOG/err
-make DESTDIR=$LFS install 2>$LOG/err
+	--enable-languages=c,c++ 
+make -j8 
+make DESTDIR=$LFS install 
 
 #As a finishing touch, create a utility symlink
-ln -sv gcc $LFS/usr/bin/cc 2>$LOG/err
+ln -sv gcc $LFS/usr/bin/cc 
 
 
 
 
 
-echo "Congratulation... You did it ..." 1>$LOG/current_pkg
+echo "Congratulation... You did it ..." 
 echo " Your Toolchain and required temporary tools are ready "
-echo " Let's move next script, the  Entering_chroot_env.sh and run this script " 1>$LOG/current_pkg
+echo " Let's move next script, the  Entering_chroot_env.sh and run this script " :
 
 #/*Author */
 #Gaurav Gautam Shakya
