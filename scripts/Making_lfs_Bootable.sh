95048c2cb358ec960570700eb729d47716d92581
Gaurav Gautam Shakya <gauravshakya4030@gmail.com>
2022-12-20 22:27:51 +0530

    System_Configuration.sh added Tue Dec 20 22:27:51 IST 2022

---
+++ scripts/Making_lfs_Bootable.sh
@@ -0,0 +1,13 @@
+/* Making the LFS System Bootable */
+
+
+
+/* Creating the /etc/fstab File */
+
+
+/* Installation of the kernel */
+
+
+
+/* Using GRUB to Set Up the Boot Process */
+
---
+++ scripts/System_Configuration.sh
@@ -0,0 +1,35 @@
+/*System Configuration */
+
+
+/* General Network Configuration */
+
+
+/* Overview of Device and Module Handling */
+
+
+
+/* Managing Devices */
+
+
+/* Configuring the system clock */
+
+
+
+/* Configuring the Linux Console */
+
+
+
+/* Configuring the System Locale */
+
+
+
+
+/* Creating the /etc/inputrc File */
+
+
+
+/* Creating the /etc/shells File */
+
+
+/* Systemd Usage and Configuration */
+
